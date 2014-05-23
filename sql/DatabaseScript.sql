1aae98e1ba753862175723d3823377f4b97c32c0
panick91 <patrick.dale.nick@gmail.com>
2014-05-23 23:44:04 +0200

    finish of detail page

--- sql/DatabaseScript.sql
+++ sql/DatabaseScript.sql
@@ -2,9 +2,9 @@ CREATE DATABASE  IF NOT EXISTS `whattocook` /*!40100 DEFAULT CHARACTER SET latin
 USE `whattocook`;
 -- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
--- Host: localhost    Database: whattocook
+-- Host: 127.0.0.1    Database: whattocook
 -- ------------------------------------------------------
--- Server version	5.5.24-log
+-- Server version	5.6.12-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -57,7 +57,7 @@ CREATE TABLE `ratings` (
   PRIMARY KEY (`ratingsId`),
   KEY `fk_receipt_rating_idx` (`receiptId`),
   CONSTRAINT `fk_receipt_rating` FOREIGN KEY (`receiptId`) REFERENCES `receipts` (`receiptId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +66,7 @@ CREATE TABLE `ratings` (
 
 LOCK TABLES `ratings` WRITE;
 /*!40000 ALTER TABLE `ratings` DISABLE KEYS */;
-INSERT INTO `ratings` VALUES (1,3,2),(2,3,2),(3,3,2),(4,3,2),(5,3,2),(6,3,2),(7,3,2);
+INSERT INTO `ratings` VALUES (1,3,2),(2,3,2),(3,3,2),(4,3,2),(5,3,2),(6,3,2),(7,3,2),(8,3,2),(9,3,2),(10,3,2),(11,3,2);
 /*!40000 ALTER TABLE `ratings` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +94,7 @@ CREATE TABLE `receipts` (
 
 LOCK TABLES `receipts` WRITE;
 /*!40000 ALTER TABLE `receipts` DISABLE KEYS */;
-INSERT INTO `receipts` VALUES (2,'Spaghetti Napoli','test',2,'spaghetti%20Napoli','15 Minutes'),(3,'Lasagne','test',4,'Placeholder','45 Minutes'),(4,'Ravioli','test',1,'Placeholder','10 Minutes'),(5,'Riz Casimir','test',2,'Placeholder','30 Minutes');
+INSERT INTO `receipts` VALUES (2,'Spaghetti Napoli','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,',1,'spaghetti%20Napoli','15 Minutes'),(3,'Lasagne','FOR RAGU: In a deep pot over low heat, warm the oil. Add the carrots and celery and sauté until the edges of the celery become translucent, about 5 minutes. Add the onion and sauté until almost translucent, about 5 minutes. Add the pancetta and cook, stirring occasionally, for 10 minutes. Then add the pork and veal and cook, stirring occasionally, until cooked but not browned, about 15 minutes. Add the tomatoes and juice and simmer, uncovered, stirring occasionally, until very thick, 2-2 1/2 hours. Season to taste with salt, pepper and nutmeg.<br/><br/>FOR SAUCE: Melt butter in a saucepan over low heat. Whisk in the flour until blended, then gradually whisk in the hot milk. Simmer, whisking constantly and scraping the sides and bottom of the pan with a spatula as necessary to avoid burning, until the raw flour flavor dissipates, about 5 minutes. Remove from the heat and season to taste with salt, white pepper and nutmeg.<br/><br/>TO ASSEMBLE: Cook lasagna noodles in large pot of boiling salted water until al dente. Drain then rinse under cold water.<br/><br/>Preheat oven to 400 degrees F. Spread 1/2 cup ragù over the bottom of 13x9-inch baking dish. Arrange a single layer of the noodles on top, do not overlap. Spread 1/3 cup of the white sauce over noodles. Top with another 1/2-2/3 cup ragù. Sprinkle with 3-4 tablespoons of the Parmesan cheese, then season to taste with salt and pepper. Repeat layering with the noodles, sauces and cheese. Arrange a final layer of noodles on top. Spread with white sauce and sprinkle with cheese.<br/><br/>Bake until sauces bubble and top is golden brown, about 40 minutes. Let stand 5 minutes. Cut into pieces and transfer to warm individual plates.',3,'Placeholder','45 Minutes'),(4,'Ravioli','test',1,'Placeholder','10 Minutes'),(5,'Riz Casimir','test',2,'Placeholder','30 Minutes');
 /*!40000 ALTER TABLE `receipts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,7 +114,7 @@ CREATE TABLE `receipts2ingredients` (
   KEY `FK_receipts2ingredients_ingredients_idx` (`ingredientId`),
   CONSTRAINT `FK_receipts2ingredients_ingredients` FOREIGN KEY (`ingredientId`) REFERENCES `ingredients` (`ingredientId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_receipts2ingredients_receipts` FOREIGN KEY (`receiptId`) REFERENCES `receipts` (`receiptId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-22 22:31:36
+-- Dump completed on 2014-05-23 23:43:26
